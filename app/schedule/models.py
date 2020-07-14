9649ab69908a423dc0f76c52396f9dcf3deb16a4
Todd Peterson <ToddPetersonGV@gmail.com>
2020-07-14 11:34:47 -0600

    Add schedule event model

--- app/schedule/models.py
+++ app/schedule/models.py
@@ -16,3 +16,24 @@ class Location(models.Model):
 
     def __str__(self):
         return f'{self.name}::{self.longitude},{self.latitude}'
+
+
+class Event(models.Model):
+    """Model representing a schedule event"""
+
+    name = models.CharField(max_length=255, blank=True)
+    info = models.CharField(max_length=255, blank=True)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+
+    location = models.ForeignKey(
+        'schedule.Location',
+        on_delete=models.CASCADE
+    )
+    vendor = models.ForeignKey(
+        'vendor.Vendor',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.date_start.date}::{self.location.name}'
--- app/schedule/tests/test_models.py
+++ app/schedule/tests/test_models.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-from schedule.models import Location
+from schedule.models import Location, Event
 from vendor.models import Vendor
 
 
@@ -54,3 +57,61 @@ class LocationTests(TestCase):
 
         expected = f'{location.name}::{location.longitude},{location.latitude}'
         self.assertEqual(str(location), expected)
+
+    def test_create_event(self):
+        """Test creating an event"""
+        vendor = sample_vendor()
+        location = Location.objects.create(
+            vendor=vendor,
+            name='New location',
+            info='Words words words',
+            longitude=1.23,
+            latitude=4.56
+        )
+        now = timezone.now()
+        later = now + timedelta(hours=4)
+        name = "Event"
+        info = "Info"
+
+        event = Event.objects.create(
+            vendor=vendor,
+            location=location,
+            date_start=now,
+            date_end=later,
+            name=name,
+            info=info
+        )
+
+        self.assertEqual(event.vendor.id, vendor.id)
+        self.assertEqual(event.location.id, location.id)
+        self.assertEqual(event.date_start, now)
+        self.assertEqual(event.date_end, later)
+        self.assertEqual(event.name, name)
+        self.assertEqual(event.info, info)
+
+    def test_event_str(self):
+        """Test that events return the correct str rep"""
+        vendor = sample_vendor()
+        location = Location.objects.create(
+            vendor=vendor,
+            name='New location',
+            info='Words words words',
+            longitude=1.23,
+            latitude=4.56
+        )
+        now = timezone.now()
+        later = now + timedelta(hours=4)
+        name = "Event"
+        info = "Info"
+
+        event = Event.objects.create(
+            vendor=vendor,
+            location=location,
+            date_start=now,
+            date_end=later,
+            name=name,
+            info=info
+        )
+
+        expected = f'{event.date_start.date}::{location.name}'
+        self.assertEqual(str(event), expected)
